082e12a219e5a950205cf937a953bad69d152056
imngui <ingui2@illinois.edu>
2024-09-16 17:51:03 -0500

    Updating ft manager.

--- ferl/feature_trace_manager.py
+++ ferl/feature_trace_manager.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String, Bool, Float64MultiArray
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 
 from sensor_msgs.msg import JointState
@@ -17,35 +18,32 @@ class FeatureTraceManager(Node):
         self.joint_state_sub = self.create_subscription(JointState, '/joint_states', self.joint_state_callback, 10)
 
         self.interaction_start_time = None
-        self.feature_trace_pub = self.create_publisher(Float64MultiArray, '/feature_trace', 10)
-        self.feature_trace_msg = Float64MultiArray()
-        self.feature_trace = []
+        self.feature_trace_pub = self.create_publisher(JointTrajectory, '/feature_trace', 10)
+        self.feature_trace_msg = JointTrajectory()
         self.track_data = False
 
 
     def joint_state_callback(self, msg):
-        # self.feature_trace_msg.joint_names = msg.name
+        self.feature_trace_msg.joint_names = msg.name
 
         if self.track_data:
-            self.feature_trace.append(np.roll(msg.position, 1))
-            # point = JointTrajectoryPoint()
-            # point.positions = msg.position
-            # point.time_from_start = int(time.time() - self.interaction_start_time)
-            # self.feature_trace_msg.points.append(point)
+            point = JointTrajectoryPoint()
+            point.positions = np.roll(msg.position, 1).tolist()
+            duration = time.time() - self.interaction_start_time
+            point.time_from_start = rclpy.time.Duration(seconds=duration).to_msg()
+            self.feature_trace_msg.points.append(point)
 
 
     def user_input_callback(self, msg):
         if msg.data == "1":
             self.track_data = True
             self.feature_trace_msg.data = []
-            # self.interaction_start_time = time.time()
+            self.interaction_start_time = time.time()
         if msg.data == "2":
             self.track_data = False
-            # self.feature_trace = [[float(self.feature_trace[i][j]) for j in range(len(self.feature_trace))] for i in range(len(self.feature_trace[0]))]
-            self.feature_trace_msg.data = self.feature_trace
             self.feature_trace_pub.publish(self.feature_trace_msg)
             self.feature_trace_msg.data = []
-            # self.interaction_start_time = None
+            self.interaction_start_time = None
 
 
 def main(args=None):
